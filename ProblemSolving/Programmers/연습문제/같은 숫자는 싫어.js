c77483b15d26099b0ed2cbd36f7c4f4bbe687bf0
Sangkun-svg <gims4752@gmail.com>
2022-06-28 04:37:13 +0900

    programmers : solve problems

---
+++ ProblemSolving/Programmers/연습문제/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr = [4, 4, 4, 3, 3]) {
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
