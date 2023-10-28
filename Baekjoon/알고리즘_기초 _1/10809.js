fe171e16b67542ebca1fe7c1ea30f4742ef13129
Sangkun-svg <gims4752@gmail.com>
2023-10-28 23:57:36 +0900

    BJ 10809

---
+++ Baekjoon/알고리즘_기초 _1/10809.js
@@ -0,0 +1,23 @@
+// 10809
+// 문자열
+
+const readline = require("readline");
+
+const solution = (input) => {
+  const result = new Array(26).fill(-1);
+  for (let idx = 0; idx < input.length; idx++) {
+    const beforeValue = input[idx].charCodeAt(0) - 97;
+    if (result[input[idx].charCodeAt(0) - 97] === -1) {
+      result.splice(input[idx].charCodeAt(0) - 97, 1, idx);
+    }
+  }
+  return result.join(" ");
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on("line", (line) => {
+  console.log(solution(line));
+});
