e65ee4809e4df7705d11f3f8da5f1c5df2fbcc7c
Sangkun-svg <gims4752@gmail.com>
2023-12-07 20:05:12 +0900

    DataStructure classes Graph Traversal

--- Baekjoon/DataStructure/Graph.js
+++ Baekjoon/DataStructure/Graph.js
@@ -1,46 +1,119 @@
 class Graph {
   constructor() {
-    this.adjecencyList = {};
+    this.adjacencyList = {};
   }
 
   addVertex(vertax) {
-    if (!this.adjecencyList[vertax]) this.adjecencyList[vertax] = [];
-  }
-
-  removeVertex(vertax) {
-    while (this.adjecencyList[vertax].length) {
-      const adjecencyVertex = this.adjecencyList[vertax].pop();
-      this.removeEdge(vertax, adjecencyVertex);
-    }
-    delete this.adjecencyList[vertax];
+    if (!this.adjacencyList[vertax]) this.adjacencyList[vertax] = [];
   }
 
   addEdge(vertax1, vertax2) {
-    this.adjecencyList[vertax1].push(vertax2);
-    this.adjecencyList[vertax2].push(vertax1);
+    this.adjacencyList[vertax1].push(vertax2);
+    this.adjacencyList[vertax2].push(vertax1);
   }
+
   removeEdge(vertax1, vertax2) {
-    this.adjecencyList[vertax1] = this.adjecencyList[vertax1].filter(
+    this.adjacencyList[vertax1] = this.adjacencyList[vertax1].filter(
       (v) => v !== vertax2
     );
-    this.adjecencyList[vertax2] = this.adjecencyList[vertax2].filter(
+    this.adjacencyList[vertax2] = this.adjacencyList[vertax2].filter(
       (v) => v !== vertax1
     );
+  }
+
+  removeVertex(vertax) {
+    while (this.adjacencyList[vertax].length) {
+      const adjecencyVertex = this.adjacencyList[vertax].pop();
+      this.removeEdge(vertax, adjecencyVertex);
+    }
+    delete this.adjacencyList[vertax];
+  }
+
+  DFSR(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    const DFS = (vertex) => {
+      if (!vertex) return null;
+      result.push(vertex);
+      visited[vertex] = true;
+      adjacencyList[vertex].forEach((v) => {
+        if (!visited[v]) {
+          return DFS(v);
+        }
+      });
+    };
+    DFS(start);
+
+    return result;
+  }
+
+  DFSI(start) {
+    const result = [];
+    const stack = [start];
+    const visited = {};
+    visited[start] = true;
+
+    while (stack.length) {
+      let vertex = stack.pop();
+      result.push(vertex);
+      this.adjacencyList[vertex].forEach((v) => {
+        if (!visited[v]) {
+          visited[v] = true;
+          stack.push(v);
+        }
+      });
+    }
+    return result;
+  }
+
+  BFS(start) {
+    const result = [];
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+    while (queue.length) {
+      const vertex = queue.shift();
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((v) => {
+        if (!visited[v]) {
+          visited[v] = true;
+          queue.push(v);
+        }
+      });
+    }
+
+    return result;
   }
 }
 
 const graph = new Graph();
-graph.addVertex("Dallas");
-graph.addVertex("Tokyo");
-graph.addVertex("Aspen");
-graph.addVertex("Los_Angeles");
-graph.addVertex("Hong_Kong");
-graph.addEdge("Dallas", "Tokyo");
-graph.addEdge("Dallas", "Aspen");
-graph.addEdge("Hong_Kong", "Tokyo");
-graph.addEdge("Hong_Kong", "Dallas");
-graph.addEdge("Los_Angeles", "Hong_Kong");
-graph.addEdge("Los_Angeles", "Aspen");
-console.log(graph);
-graph.removeVertex("Hong_Kong");
-console.log(graph);
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+// console.log(graph.DFSR("A"))
+// console.log(graph.DFSI("A"))
+console.log(graph.BFS("A"))
+
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
