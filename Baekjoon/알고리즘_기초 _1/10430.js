cdee1e406139334e1d1e41ee6b4e85ec60debd39
Sangkun-svg <gims4752@gmail.com>
2023-12-23 04:01:15 +0900

    delete unnecessary files

--- Baekjoon/알고리즘_기초 _1/10430.js
+++
@@ -1,9 +0,0 @@
-// 10430
-
-const fs = require("fs");
-const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
-const input = inputData.map((el) => parseInt(el));
-console.log((input[0] + input[1]) % input[2]);
-console.log(((input[0] % input[2]) + (input[1] % input[2])) % input[2]);
-console.log((input[0] * input[1]) % input[2]);
-console.log(((input[0] % input[2]) * (input[1] % input[2])) % input[2]);
--- Baekjoon/알고리즘_기초 _1/2743.js
+++
@@ -1,6 +0,0 @@
-// 2743
-// 문자열
-
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-console.log(input[0].length);
